312f7ea7154c0c06b1505894ba1259a2e5708f2f
Lin-A1 <860693883@qq.com>
2025-04-11 17:01:37 +0800

    update

--- run.sh
+++ run.sh
@@ -9,3 +9,11 @@ pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 -f https://mirror
 
 # 安装依赖
 pip install -e .
+
+# 拉取mineru模型
+pip install modelscope
+wget https://gcore.jsdelivr.net/gh/opendatalab/MinerU@master/scripts/download_models.py -O download_models.py
+
+# 进入download_models.py文件所在目录
+cd scripts
+python download_models.py
